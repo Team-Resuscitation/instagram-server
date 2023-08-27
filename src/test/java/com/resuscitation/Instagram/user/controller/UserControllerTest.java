0731530b11e948ad839e99106348582293620fc8
Chaemin Yooun <93826302+oyune@users.noreply.github.com>
2023-08-27 13:47:15 +0900

    UserController 통합 테스트 작성
    1. 회원 탈퇴 케이스
    2. 회원 정보 수정 케이스
    3. 비밀번호 변경 케이스

--- src/test/java/com/resuscitation/Instagram/user/controller/UserControllerTest.java
+++ src/test/java/com/resuscitation/Instagram/user/controller/UserControllerTest.java
@@ -2,14 +2,19 @@ package com.resuscitation.Instagram.user.controller;
 
 import com.resuscitation.Instagram.user.dto.JwtDto;
 import com.resuscitation.Instagram.user.dto.LoginFormDto;
+import com.resuscitation.Instagram.user.dto.PasswordChangeFormDto;
 import com.resuscitation.Instagram.user.dto.RegisterFormDto;
 import com.resuscitation.Instagram.user.entity.UserEntity;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.springframework.test.util.AssertionErrors.*;
 
@@ -45,7 +50,7 @@ class UserControllerTest {
 
         // THEN
         assert (response.getStatusCode().equals(HttpStatusCode.valueOf(200)));
-        assertNotNull("", response.getBody().getToken());
+        assertNotNull("회원가입 실패", response.getBody().getToken());
     }
 
     @Test
@@ -58,7 +63,7 @@ class UserControllerTest {
 
         // THEN
         assert (response.getStatusCode().equals(HttpStatusCode.valueOf(200)));
-        assertNotNull("", response.getBody().getToken());
+        assertNotNull("로그인 실패", response.getBody().getToken());
 
     }
 
@@ -76,27 +81,75 @@ class UserControllerTest {
         UserEntity responseUser = response.getBody();
 
         // Then
+        assert (response.getStatusCode().equals(HttpStatusCode.valueOf(200)));
         assertEquals("이메일이 일치하지 않습니다.", email, responseUser.getEmail());
         assertEquals("이름이 일치하지 않습니다.", name, responseUser.getName());
-        assertNotEquals("비밀번호가 암호화 되지 않았습니다.", password, responseUser.getPassword());
+        assertNotEquals("비밀번호가 암호화되지 않았습니다.", password, responseUser.getPassword());
 
     }
 
-    @Disabled("Disabled until implement")
     @Test
     @DisplayName("회원 탈퇴 - 성공")
     void deleteUser() {
+        //Given
+        LoginFormDto loginFormDto = new LoginFormDto(email, password);
+        ResponseEntity<JwtDto> loginResponse = userController.login(loginFormDto);
+
+        //When
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader("Authorization", loginResponse.getBody().getToken());
+        ResponseEntity<UserEntity> response = userController.whois(request);
+        UserEntity responseUser = response.getBody();
+
+        //Then
+        assert (response.getStatusCode().equals(HttpStatusCode.valueOf(200)));
+        assertNotEquals("비밀번호가 암호화되지 않았습니다.", password, responseUser.getPassword());
     }
 
-    @Disabled("Disabled until implement")
     @Test
     @DisplayName("회원 정보 수정 - 성공")
     void updateProfile() {
+        //Given
+        LoginFormDto loginFormDto = new LoginFormDto(email, password);
+        ResponseEntity<JwtDto> loginResponse = userController.login(loginFormDto);
+
+        //When
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        ResponseEntity<UserEntity> response = userController.whois(request);
+        UserEntity responseUser = response.getBody();
+
+        //Then
+        assert (response.getStatusCode().equals(HttpStatusCode.valueOf(200)));
+        assertNotEquals("비밀번호가 암호화되지 않았습니다.", password, responseUser.getPassword());
+        assertNotNull("이메일은 비워둘 수 없습니다.", email);
+        assertNotNull("이름은 비워둘 수 없습니다.", name);
     }
 
-    @Disabled("Disabled until implement")
     @Test
     @DisplayName("비밀번호 변경 - 성공")
     void updatePassword() {
+        //Given
+        LoginFormDto loginFormDto = new LoginFormDto(email, password);
+        ResponseEntity<JwtDto> loginResponse = userController.login(loginFormDto);
+        String newPassword = "PassWord!!!";
+        String rePassword = "PassWord!!!";
+        PasswordChangeFormDto passwordChangeFormDto = new PasswordChangeFormDto(password, newPassword, rePassword);
+        String passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+
+        //When
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader("Authorization", loginResponse.getBody().getToken());
+        ResponseEntity<UserEntity> response = userController.whois(request);
+        UserEntity responseUser = response.getBody();
+
+        //Then
+        assert (response.getStatusCode().equals(HttpStatusCode.valueOf(200)));
+        assertNotEquals("비밀번호가 암호화되지 않았습니다.", password, responseUser.getPassword());
+        assertTrue("비밀번호는 반드시 한글자 이상의 대소문자 및 특수기호를 포함한 8자 이상으로 구성되어야 합니다.",
+                newPassword.matches(passwordPattern));
+        assertNotEquals("기존과 동일한 비밀번호입니다.",
+                passwordChangeFormDto.getNewPassword(), responseUser.getPassword());
+        assertEquals("비밀번호가 일치하지 않습니다.",
+                rePassword, passwordChangeFormDto.getRePassword());
     }
 }
