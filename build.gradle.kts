edcd19156669bcd8a44c24bc4d4cde745ab02303
Chaemin Yooun <cinnamein@gmail.com>
2025-02-06 01:12:17 +0900

    [Refactor] build.gradle 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,53 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+<<<<<<< Updated upstream
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("plugin.jpa") version "1.9.25"
 }
 
 
+=======
+    id("org.springframework.boot") version "3.4.1"
+    id("io.spring.dependency-management") version "1.1.7"
+    kotlin("plugin.jpa") version "1.9.25"
+}
+
+>>>>>>> Stashed changes
 group = "com.resuscitation"
 version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
+<<<<<<< Updated upstream
         languageVersion = JavaLanguageVersion.of(17)
     }
 }
 
 
+=======
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+>>>>>>> Stashed changes
 repositories {
     mavenCentral()
 }
 
 dependencies {
+<<<<<<< Updated upstream
+=======
+    // Spring Boot
+>>>>>>> Stashed changes
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+<<<<<<< Updated upstream
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -36,10 +56,37 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     val springDocVersion = "2.6.0"
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocVersion")
+=======
+
+    // database driver
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+
+    // redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // JJWT
+>>>>>>> Stashed changes
     val jjwtVersion = "0.12.6"
     implementation("io.jsonwebtoken:jjwt-api:$jjwtVersion")
     implementation("io.jsonwebtoken:jjwt-impl:$jjwtVersion")
     implementation("io.jsonwebtoken:jjwt-jackson:$jjwtVersion")
+<<<<<<< Updated upstream
+=======
+
+    // oauth
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
+    // test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // OpenAPI
+    val swaggerVersion = "2.8.3"
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$swaggerVersion")
+>>>>>>> Stashed changes
 }
 
 kotlin {
