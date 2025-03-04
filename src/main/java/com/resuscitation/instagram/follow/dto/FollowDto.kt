e81d26a146dec3c2dc2ab4af52cf1a19fc95959c
Chaemin Yooun <cinnamein@gmail.com>
2025-03-05 02:46:18 +0900

    Delete: follow package

--- src/main/java/com/resuscitation/instagram/follow/dto/FollowDto.kt
+++
@@ -1,7 +0,0 @@
-package com.resuscitation.instagram.follow.dto
-
-data class FollowDto(
-    val profileImageUrl: String,
-    val nickname: String,
-    val name: String,
-)
--- src/main/java/com/resuscitation/instagram/follow/service/FollowInfoService.kt
+++
@@ -1,9 +0,0 @@
-package com.resuscitation.instagram.follow.service
-
-import com.resuscitation.instagram.follow.dto.FollowDto
-
-interface FollowInfoService {
-    fun getFollowers(nickname: String): List<FollowDto>
-
-    fun getFollowings(nickname: String): List<FollowDto>
-}
